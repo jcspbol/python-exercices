deaf944f7f6141bfa80cb48353e446cf3c8d9b5e
Juan Carlos Serna <jcspbol@gmail.com>
2024-06-11 10:13:14 -0400

    Num positivo

---
+++ NumeroPositivo.py
@@ -0,0 +1,7 @@
+num=float(input("Ingrese un numero: "))
+if num>0:
+    print("Es numero positivo: ",num)
+elif num==0:
+    print("El numeor es Cero: ",num)
+else:
+    print("El numero es negativo: ",num)
